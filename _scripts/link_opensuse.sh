58f8b907a80e99795ff1618bd48be2a1cf78d013
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-05-06 21:34:56 +0200

    add scripts

---
+++ _scripts/link_opensuse.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/waybar ~/.config/waybar
+
+ln -s ~/dotfiles/neovide/neovide-settings.json ~/.local/share/neovide/neovide-settings.json
+
+ln -s ~/dotfiles/gitconfig/.gitconfig ~/.gitconfig
