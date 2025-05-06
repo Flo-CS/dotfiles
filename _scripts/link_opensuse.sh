5b904f551e69ddca6a898af91e9f314d695c1d30
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-05-06 22:03:01 +0200

    update opensuse script

--- _scripts/link_opensuse.sh
+++ _scripts/link_opensuse.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-ln -s ~/dotfiles/hypr ~/.config/hypr
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -sT ~/dotfiles/hypr ~/.config/hypr
+ln -sT ~/dotfiles/nvim ~/.config/nvim
+ln -sT ~/dotfiles/waybar ~/.config/waybar
 
-ln -s ~/dotfiles/neovide/neovide-settings.json ~/.local/share/neovide/neovide-settings.json
-
-ln -s ~/dotfiles/gitconfig/.gitconfig ~/.gitconfig
+ln -sT ~/dotfiles/gitconfig/opensuse/.gitconfig ~/.gitconfig
