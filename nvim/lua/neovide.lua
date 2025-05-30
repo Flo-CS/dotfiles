3765dada318577c20616346d910578d679e455af
Florian Etrillard <fetrillard@vaduoconsulting.com>
2025-05-30 12:12:54 +0200

    reduce neovide cursor animation duration

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -1,8 +1,10 @@
 if vim.g.neovide then
+  print 'Running in Neovide'
   vim.o.guifont = 'JetBrainsMono Nerd Font'
   vim.opt.linespace = 3
   -- Styling
   vim.g.neovide_scroll_animation_length = 0.1
+  vim.g.neovide_cursor_animation_length = 0.075
   vim.g.neovide_cursor_trail_size = 0.1
   -- vim.g.neovide_transparency = 0.975
   -- vim.g.neovide_normal_opacity = 0.975
