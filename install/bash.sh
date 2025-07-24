2c8b3fd073f835f3f104480fff31cbcffda86fe3
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-07-24 09:25:02 +0200

    fix bash profile symlink

--- install/bash.sh
+++ install/bash.sh
@@ -3,5 +3,5 @@ create_dotfiles_symlink .bashrc ~/.bashrc-default
 insert_content_with_marker ~/.bashrc "bashrc-default" "source ~/.bashrc-default"
 
 section "Bash Profile"
-create_dotfiles_symlink .profile ~/.profile-default
+create_dotfiles_symlink .bash_profile ~/.profile-default
 insert_content_with_marker ~/.bash_profile "profile-default" "source ~/.profile-default"
