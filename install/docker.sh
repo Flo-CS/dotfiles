04e7832dbf87bca465a9454c1ca887272a1f4d55
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-07-24 11:37:45 +0200

    add lazydocker

--- install/docker.sh
+++ install/docker.sh
@@ -1,3 +1,4 @@
 section "Docker"
 install_packages docker docker-compose
+install_yay_packages lazydocker
 sudo systemctl enable --now docker.socket # Start on first usage, different from .service which start on boot
