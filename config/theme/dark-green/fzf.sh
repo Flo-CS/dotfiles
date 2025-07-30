d1b411aa565e64bd3e15055de2920269871f96aa
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-07-30 21:05:39 +0200

    add dark green theme

---
+++ config/theme/dark-green/fzf.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="
+	--color=fg:#666666,bg:#0a0a0a,hl:#4a9b5e
+	--color=fg+:#e8e8e8,bg+:#1e1e1e,hl+:#4a9b5e
+	--color=border:#3a3a3a,header:#5a8a6b,gutter:#0a0a0a
+	--color=spinner:#b8860b,info:#5a8a6b
+	--color=pointer:#4a9b5e,marker:#d97757,prompt:#666666"
