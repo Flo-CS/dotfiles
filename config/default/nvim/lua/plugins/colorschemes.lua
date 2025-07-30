9110682f4af1663ea8ea7a4f0b75b4bf3ea7bc72
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-07-30 21:17:55 +0200

    remove theme background

--- config/default/nvim/lua/plugins/colorschemes.lua
+++ config/default/nvim/lua/plugins/colorschemes.lua
@@ -15,5 +15,13 @@ return {
     name = 'rose-pine',
     priority = 1000,
     lazy = false,
+    config = function()
+      require('rose-pine').setup {
+        styles = {
+          transparency = true,
+        },
+      }
+      vim.cmd.colorscheme 'rose-pine'
+    end,
   },
 }
