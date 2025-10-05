00dce0738ebbc4a9ae67be6d89290338ae70aa86
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-10-05 15:52:33 +0200

    stuff

--- files/default/bash/functions.sh
+++ files/default/bash/functions.sh
@@ -121,6 +121,6 @@ nvf() {
 }
 
 reboot_to_windows() {
-	windows_title=$(grep -i windows /boot/grub/grub.cfg | cut -d "'" -f 2)
+	windows_title=$(sudo grep -i windows /boot/grub/grub.cfg | cut -d "'" -f 2)
 	sudo grub-reboot "$windows_title" && sudo reboot
 }
